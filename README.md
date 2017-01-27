b98c001ad406f6bfecf0684fef35284a1322922a
dhershman1 <dustinh17@gmail.com>
2017-01-27 10:20:39 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+**Sass-Pack**
+=============
+
+A simple CLI system for compiling sass down to CSS and builds a css_manifest for webpack this will run completey async and uses promise based calls to compile our sass to css
+
+Since this module is not run on npm yet, you have to use `npm ln` in the sasspack directory to make sure it creates a link to your project.
+
+Command & Options
+--------
+
+ - `sass-pack [options]` - Run sass-pack using your options
+ - `-s` - Set the source path of page based scss `Required`
+ - `-t` - Set the path to your `theme` scss `Required`
+ - `-m  - Set the path of your `css_manifest.json` `Required`
+ - `-o` - Set the path for the output css `Required`
+
+Example:
+> sass-pack -o public/css -s src/app -t public/scss/themes -m src/config/css_manifest.json
+
+Requirements
+------------
+
+ - `node-sass` - the node scss compiler library
+ - `minimist` - module to setup cli support
+ - `fs-promise` - module to use promises with the file system library, also include `fs-extra`
+ - `globby` - module to allow us to use promise based globs
+ - `progress` - module to keep track of progress in our terminal
---
+++ index.js
@@ -0,0 +1,87 @@
+#! /usr/bin/env node
+
+// This needs to be reformatted and re worked to be better. -Dustin
+
+'use strict';
+
+const fsp = require('fs-promise');
+const sass = require('node-sass');
+const path = require('path');
+const parsedArgs = require('minimist')(process.argv.slice(2));
+const ProgressBar = require('progress');
+const globby = require('globby');
+
+function sassPack(opts) {
+
+	fsp.mkdirp(opts.o, err => {
+		if (err) {
+			throw err;
+		}
+	});
+	let bar = {};
+
+	function compile(file) {
+
+		return new Promise((resolve, reject) => {
+			let name = path.parse(file).name;
+
+			sass.render({
+				file: file
+			}, (compileErr, result) => {
+				if (compileErr) {
+					reject(compileErr);
+				}
+				resolve({
+					name,
+					result
+				});
+			});
+		});
+	}
+
+
+	globby([opts.t, opts.s])
+		.then(paths => {
+			const filteredPaths = paths.filter(file => {
+				return !file.includes('framework');
+			});
+
+			bar = new ProgressBar('Compiling SASS [:bar] :current/:total :elapsed :percent', {
+				total: filteredPaths.length,
+				width: 20
+			});
+
+			return Promise.all(filteredPaths.map(file => {
+				return compile(file);
+			}));
+		})
+		.then(data => {
+			return Promise.all(data.map(({ name, result }) => {
+				bar.tick();
+
+				return fsp.writeFile(path.resolve(opts.o, `${name}.css`), result.css);
+			}));
+		})
+		.then(() => {
+			return globby(path.join(`${opts.o}`, '*.css'));
+		})
+		.then(paths => {
+			let obj = {};
+
+
+			paths.map(file => {
+				obj[path.parse(file).name] = file;
+
+				return obj;
+			});
+
+			return fsp.writeJson(opts.m, obj);
+		})
+		.catch(err => {
+			throw err;
+		});
+}
+
+sassPack(parsedArgs);
+module.exports = sassPack;
+
